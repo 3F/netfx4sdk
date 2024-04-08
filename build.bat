bbffccf2b75324306aaebd3bbe1e30f483fde023
Denis Kuzmin (github/3F) <x-3F@outlook.com>
2024-04-08 21:43:41 +0300

    Updated hMSBuild 2.4.1

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 
-call .tools\hMSBuild -GetNuTool /p:wpath="%cd%" /p:ngconfig=".tools\packages.config" /nologo /v:m /m:7 & (
+call .tools\hMSBuild ~x -GetNuTool & (
     if [%~1]==[#] exit /B 0
 )
 call packages\vsSolutionBuildEvent\cim.cmd /v:m /m:7 /p:Configuration="Release" || (
-    echo. Failed 1>&2
+    echo Failed>&2
     exit /B 1
 )
 exit /B 0
